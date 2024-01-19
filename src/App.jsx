1e0e6f2aa0365f85f94f2514fadd1fa00d927691
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-01-19 18:59:51 +0530

    feat: add Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Loader from './components/Loader';
 
 function App() {
   const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=2');
+      // Get pokemon name and pokemon urls here
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=15');
       const data = await response.json();
       const pokeArray = await data.results;
-      const updatedData = await Promise.all(
+
+      // Return pokemon names and image urls
+      const updatedPokeArray = await Promise.all(
         pokeArray.map(async (pokemon) => {
           const name = pokemon.name;
           const imgUrl = await getPokemonImg(pokemon.url);
           return { name, imgUrl };
         }),
       );
-      setPokeData(updatedData);
+
+      setPokeData(updatedPokeArray);
+      setLoading(false);
     };
     fetchData();
+    // Fetch data only once when the component mounts
   }, []);
 
+  // Fetch pokemon image url
   const getPokemonImg = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
@@ -28,16 +37,20 @@ function App() {
   };
 
   return (
-    <div>
-      {pokeData.map((pokemon) => {
-        return (
-          <div key={pokemon.name}>
-            <p>{pokemon.name}</p>
-            <img src={pokemon.imgUrl} alt={pokemon.name} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {loading ? (
+        <Loader/>
+      ) : (
+        pokeData.map((pokemon) => {
+          return (
+            <div key={pokemon.name}>
+              <p>{pokemon.name}</p>
+              <img src={pokemon.imgUrl} alt={pokemon.name} />
+            </div>
+          );
+        })
+      )}
+    </>
   );
 }
 
